19525a142b548d9690aa550f36bfaa131dd9ac59
RYAN JAMES <realcoder2023@gmail.com>
2025-11-28 14:51:57 -0500

    made function insided of calling cvode into function

--- fileCreatedFile.ts
+++ fileCreatedFile.ts
@@ -7,41 +7,44 @@ import fs from "fs";
 const linesFile = createDependencies<string>("libsNode.log");
 const lines: string[] = linesFile.split(/\r?\n/);
 
-const createInstalledPackages = () => {
-  const args = process.argv.slice(2);
-  if (args.toString() == "--File") {
-    fs.writeFileSync("installedPackakes.log", linesFile.toString(), "utf-8");
-  } else {
-    return;
+const createInstallArgs = () => {
+  const argsPara = process.argv.slice(2);
+
+  if (argsPara.toString() == "--install") {
+    let messageIndexer = linesFile.toString();
+    fs.writeFileSync("installedPackakes.log", messageIndexer, "utf-8");
   }
 };
 
-for (const line of lines) {
-  exec(`npm install ${line}`, (error, stdout, stderr) => {
-    if (error) {
-      console.log("error: " + error.message);
-      return;
-    }
+const installCreatedPackagesFromArgs = () => {
+  for (const line of lines) {
+    exec(`npm install ${line}`, (error, stdout, stderr) => {
+      if (error) {
+        console.log("error: " + error.message);
+        return;
+      }
 
-    if (stderr) {
-      console.log("Error: " + stderr);
-      return;
-    }
-    console.log("input: " + stdout);
+      if (stderr) {
+        console.log("Error: " + stderr);
+        return;
+      }
+      console.log("input: " + stdout);
+    });
 
-    createInstalledPackages();
-  });
+    exec(`npm init -y`, (error, stdout, stderr) => {
+      if (error) {
+        console.log("error: " + error.message);
+        return;
+      }
 
-  exec(`npm init -y`, (error, stdout, stderr) => {
-    if (error) {
-      console.log("error: " + error.message);
-      return;
-    }
+      if (stderr) {
+        console.log("Error: " + stderr);
+        return;
+      }
+      console.log("input: " + stdout);
+    });
+  }
+};
 
-    if (stderr) {
-      console.log("Error: " + stderr);
-      return;
-    }
-    console.log("input: " + stdout);
-  });
-}
+createInstallArgs();
+installCreatedPackagesFromArgs();
