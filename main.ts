a7a255300f5ba6047b4adbdaccd86e41c0226102
RYAN JAMES <realcoder2023@gmail.com>
2025-12-02 16:45:44 -0500

    added classes for more readable code

--- main.ts
+++ main.ts
@@ -4,9 +4,33 @@ export type FileArgs = {
   file: string;
 };
 
-function createDependencies<T>(readableFile: FileArgs | string) {
+interface IFileType {
+  file: FileArgs;
+
+  createFile(): void;
+}
+
+class Data implements IFileType {
+  public file: FileArgs;
+  constructor(file: FileArgs) {
+    this.file = file;
+  }
+
+  createFile(): void {
+    console.log("Created: " + this.file);
+  }
+}
+
+function createDependencies<T>(readableFile: IFileType | string) {
   const readFile = fs.readFileSync(readableFile.toString(), "utf-8");
-  return readFile;
+
+  const fileName = {
+    file: readableFile,
+  } as unknown as FileArgs;
+
+  const fileArgs = new Data(fileName);
+
+  return fileArgs.file;
 }
 
 export default createDependencies;
